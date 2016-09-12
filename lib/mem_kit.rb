4c9d340ad679e5fb138c57c9729eb73786a041a4
vaughanbrittonsage <vaughan.britton@sage.com>
2016-09-12 09:12:05 +0100

    added missing require for objspace

--- lib/mem_kit.rb
+++ lib/mem_kit.rb
@@ -1,3 +1,4 @@
+require 'objspace'
 require_relative 'mem_kit/version'
 require_relative 'mem_kit/profiler'
 require_relative 'mem_kit/cleaner'
--- lib/mem_kit/version.rb
+++ lib/mem_kit/version.rb
@@ -1,3 +1,3 @@
 module MemKit
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
